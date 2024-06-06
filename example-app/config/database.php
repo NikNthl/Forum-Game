f249f4de92b9328b7873cfd3a8408937e8c647f0
melfanny <melfanny.535230152@stu.untar.ac.id>
2024-06-06 22:55:42 +0700

    db

--- example-app/config/database.php
+++ example-app/config/database.php
@@ -86,7 +86,7 @@ return [
             'port' => env('DB_PORT', '8000'),
             'database' => env('DB_DATABASE', 'forumgame'),
             'username' => env('DB_USERNAME', 'postgres'),
-            'password' => env('DB_PASSWORD', 'fan1'),
+            'password' => env('DB_PASSWORD', ' '),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
