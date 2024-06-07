5a835f9448d5fdeb9f15af10cde61eb0a432fc87
OctoNik <nikolaus.535230113@stu.untar.ac.id>
2024-06-07 20:33:50 +0700

    route

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\homeController;
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\registerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [homeController::class, 'index']);
+Route::get('/home', [homeController::class, 'index']);
+
+Route::get ('/login', [loginController::class, 'index']);
+
+Route::get ('/register', [registerController::class, 'index']);
