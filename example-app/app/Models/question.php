b5ddc382dc96ae216d5d4f1b077852b1cafa3cff
melfanny <melfanny.535230152@stu.untar.ac.id>
2024-06-10 11:42:44 +0700

    tambahan

--- example-app/app/Models/question.php
+++ example-app/app/Models/question.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-use Illuminate\Database\Eloquent\Model\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class question extends Model
