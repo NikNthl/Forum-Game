25f2639f9fe0b07f3182199323d11d56f2f07620
pqmlw <paquita.535230062@stu.untar.ac.id>
2024-06-11 13:09:23 +0700

    tadi ada footer bantet

--- example-app/resources/views/login.blade.php
+++ example-app/resources/views/login.blade.php
@@ -1,56 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+@include('header')
+@include('menu')
 
-@section('title')
-    PIMPAN
-@endsection
-
-@include ('header')
-@include ('menu')
 <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>@yield('title')</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
-  <div class="container">
-    <div class="row">
-      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div class="card border-0 shadow rounded-3 my-5">
-          <div class="card-body p-4 p-sm-5">
-            <h5 class="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
-            @if(session('error'))
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>@yield('title')</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div class="card border-0 shadow rounded-3 my-5">
+                    <div class="card-body p-4 p-sm-5">
+                        <h5 class="card-title text-center mb-5 fw-light fs-5">Sign In</h5>
+                        @if(session('error'))
+                        <!-- Tambahkan pesan kesalahan -->
+                        <div>{{ session('error') }}</div>
+                        @endif
+                        <form method="POST" action="/">
+                            @csrf
+                            <div class="form-floating mb-3">
+                                <input type="text" name="username" class="form-control" id="floatingInput"
+                                    placeholder="Your Name">
+                                <label for="floatingInput">Username</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="password" name="password" class="form-control" id="floatingPassword"
+                                    placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                            <div class="d-grid">
+                                <input type=submit class="btn btn-primary btn-login text-uppercase fw-bold"
+                                    value="Sign In">
+                            </div>
+                            <!-- Tambahkan tautan "Register now!" -->
+                            <div class="mt-3 text-center">
+                                <p>Don't have an account? <a href="/register">Register now!</a></p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            @endif
-            <form method="POST" action="/">
-               @csrf
-              <div class="form-floating mb-3">
-                <input type="text" name="username" class="form-control" id="floatingInput" placeholder="Your Name">
-                <label for="floatingInput">Username</label>
-              <div class="form-floating mb-3">
-                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
-              </div>
-              <div>
-              <label for="remember">Remember Me</label>
-              <input type="checkbox" id="remember" name="remember">
-              </div>
-              <div class="d-grid">
-                <input type = submit  class="btn btn-primary btn-login text-uppercase fw-bold" value = "Sign In">
-              </div>
-            </form>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
+
+    @include('footer')
 </body>
 
-
-@include('footer')
 </html>
