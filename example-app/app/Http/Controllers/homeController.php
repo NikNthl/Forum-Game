2277e6144a844bd84a881cd7fe82861b546df8fb
OctoNik <nikolaus.535230113@stu.untar.ac.id>
2024-06-17 02:30:56 +0700

    homecontroller

--- example-app/app/Http/Controllers/homeController.php
+++ example-app/app/Http/Controllers/homeController.php
@@ -8,7 +8,7 @@ class homeController extends Controller
 {
     public function index()
     {
-        $questions = Question::all();
+        $questions = question::all();
         return view('home', compact('questions'));
     }
 }
